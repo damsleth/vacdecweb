059c6f84db99e737b02c36a2f1812f298bfd520b
Carl Joakim Damsleth <kim@damsleth.no>
2021-09-23 15:16:01 +0200

    removed copy button

--- public/js/vacdecweb.js
+++ public/js/vacdecweb.js
@@ -7,7 +7,6 @@ function init() {
 
 function imageDropped() {
   document.getElementById("qrdata").value = ""
-  document.getElementById("copydatabtn").value = "Copy JSON"
   document.getElementById("spinner").style.display = "inline"
 }
 
@@ -27,7 +26,6 @@ function doneDecoding(qrjson) {
     document.getElementById("qrdata").value = "Error decoding image - are you sure there's a vaccine passport QR code in this photo?"
   } else {
     document.getElementById("qrdata").value = qrjson
-    document.getElementById("copydatabtn").style.display = "inline"
   }
 }
 
@@ -44,41 +42,5 @@ function toggleDarkMode() {
   localStorage["darkmode"] == "true" ? localStorage["darkmode"] = "false" : localStorage["darkmode"] = "true"
 };
 
-function copyQRData() {
-  let qrtxt = document.getElementById("qrdata")
-  qrtxt.select()
-  qrtxt.setSelectionRange(0, 99999) /* For mobile devices */
-  navigator.clipboard.writeText(qrtxt.value)
-  iosCopyToClipboard(qrtxt)
-  document.getElementById("copydatabtn").value = "Copied to clipboard"
-  clearSelection()
-}
-
-
-function iosCopyToClipboard(el) {
-  var oldContentEditable = el.contentEditable,
-    oldReadOnly = el.readOnly,
-    range = document.createRange()
-
-  el.contentEditable = true
-  el.readOnly = false
-  range.selectNodeContents(el)
-
-  var s = window.getSelection()
-  s.removeAllRanges()
-  s.addRange(range)
-
-  el.setSelectionRange(0, 999999) // A big number, to cover anything that could be inside the element.
-
-  el.contentEditable = oldContentEditable
-  el.readOnly = oldReadOnly
-
-  document.execCommand('copy')
-}
-
-function clearSelection() {
-  if (window.getSelection) { window.getSelection().removeAllRanges() }
-  else if (document.selection) { document.selection.empty() }
-}
 
 (() => init())()
